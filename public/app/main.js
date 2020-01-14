fcb98e12607ae4dbbffb2e088b203c9b30cfdfe4
major-phyo-san <phyojupiter9@gamil.com>
2020-01-14 20:20:38 +0630

    ServiceWorker registration added on 2020-01-13

--- public/app/main.js
+++ public/app/main.js
@@ -591,3 +591,15 @@ cloudyWeather.controller("mainController", function($scope, $mdSidenav, $window,
 
 });
 
+if('serviceWorker' in navigator){
+	window.addEventListener('load', function(){
+		navigator.serviceWorker.register('/service-worker.js').
+		then(function(registration){
+			console.log("success");
+
+		}, function(err){
+			console.log("error");
+		});
+	});
+}
+
